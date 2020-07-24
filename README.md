ec6f0a1c11fcdff722eb181272f33167a46c4aa9
jwin4740@gmail.com <jwin4740@gmail.com>
2020-07-24 09:58:55 -0400

    created .env file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # nodejs_fastify_tiktok_jjw
 API in nodejs and fastify that allows users to upload videos and connect them to tiktok
+
+- create .env file with POSTGRES PROPS
--- src/plugins/postgres.db.js
+++ src/plugins/postgres.db.js
@@ -5,11 +5,11 @@ const pg = require('fastify-postgres')
 
 module.exports = fp(async (fastify, opts) => {
   const postgresOpts = Object.assign({}, {
-    user: process.env.POSTGRES_USER || 'postgress',
-    host: process.env.POSTGRES_HOST || 'localhost',
-    database: process.env.POSTGRES_DATABASE || 'tiktok',
-    password: process.env.POSTGRES_PASSWORD || 'root',
-    port: process.env.POSTGRES_PORT || '3211'
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DATABASE,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
   }, opts.pg)
 
   fastify.register(pg, postgresOpts)
